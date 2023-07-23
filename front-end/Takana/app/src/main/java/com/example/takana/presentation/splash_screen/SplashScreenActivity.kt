789e23a750a9ec9364cbcc9a6dc0acb22eb67920
doloshabell <22248628@mail.btpnsyariah.com>
2023-07-23 15:31:25 +0700

    wip: login and register view

--- front-end/Takana/app/src/main/java/com/example/takana/presentation/splash_screen/SplashScreenActivity.kt
+++ front-end/Takana/app/src/main/java/com/example/takana/presentation/splash_screen/SplashScreenActivity.kt
@@ -1,23 +1,30 @@
-package com.example.takana
+package com.example.takana.presentation.splash_screen
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.takana.MainActivity
+import com.example.takana.R
+import com.example.takana.databinding.ActivitySplashScreenBinding
+import com.example.takana.presentation.login.LoginActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class SplashScreenActivity : AppCompatActivity() {
+
+     lateinit var binding: ActivitySplashScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         supportActionBar?.hide()
 
         CoroutineScope(Dispatchers.Main).launch {
             delay(5000L)
-            startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+            startActivity(Intent(this@SplashScreenActivity, LoginActivity::class.java))
             finish()
         }
     }
