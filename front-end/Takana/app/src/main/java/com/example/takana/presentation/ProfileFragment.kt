af12490c492146a17ea082f710463ed77b3a30f2
doloshabell <22248628@mail.btpnsyariah.com>
2023-07-24 20:46:14 +0700

    wip: add display for profile

--- front-end/Takana/app/src/main/java/com/example/takana/presentation/ProfileFragment.kt
+++ front-end/Takana/app/src/main/java/com/example/takana/presentation/ProfileFragment.kt
@@ -6,17 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.takana.R
+import com.example.takana.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+
+    lateinit var binding: FragmentProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
+        setupContent()
+        return binding.root
+    }
+
+    private fun setupContent() {
+        binding.apply {
+            tvUsername.text = getString(R.string.username_x, "doloshab")
+        }
     }
 
 }
